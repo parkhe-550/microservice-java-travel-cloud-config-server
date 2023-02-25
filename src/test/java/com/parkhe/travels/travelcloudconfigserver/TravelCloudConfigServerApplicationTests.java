8c85633b83aabd62218e4ba98d5843d9698f34fd
parkhe-550 <prof.parkhe.550@gmail.com>
2023-02-25 15:25:04 +0530

    First Commit

---
+++ src/test/java/com/parkhe/travels/travelcloudconfigserver/TravelCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhe.travels.travelcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
